8a8cab892d27d180658a840734cb97792f989f40
guttman10 <guttman99@gmail.com>
2020-01-24 01:15:00 +0200

    Webcam stream count faces now

--- Camera Handler.py
+++ Camera Handler.py
@@ -1,11 +1,12 @@
 import cv2
+import face_recognition
 #pip install opencv-python
-import numpy as np
 cap = cv2.VideoCapture(0)
-print(cap.read())
 while(True):
     ret, frame = cap.read()
     cv2.imshow('frame',frame)
+    faces = face_recognition.face_locations(frame)
+    print(f'Face count: {len(faces)}')
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
