9edb237ceaa2660cccc79b78aa3487dd9a3ad5c0
guttman10 <guttman99@gmail.com>
2020-01-24 13:12:04 +0200

    Func update
    now reads all camera from computer

--- Camera Handler.py
+++ Camera Handler.py
@@ -1,14 +1,33 @@
 import cv2
 import face_recognition
 #pip install opencv-python
-cap = cv2.VideoCapture(0)
+cap = []
+faceCounts = []
+camLoop = True
+i = 0
+while camLoop is not False:
+    capTest = cv2.VideoCapture(i)
+    camLoop, spoof = capTest.read()
+    if camLoop is False:
+        break
+    cap.insert(i, cv2.VideoCapture(i))
+    faceCounts.insert(i, 0)
+    i += 1
+    capTest.release()
+
+
 while(True):
-    ret, frame = cap.read()
-    cv2.imshow('frame',frame)
-    faces = face_recognition.face_locations(frame)
-    print(f'Face count: {len(faces)}')
+    i = 0
+    while i < len(cap):
+        ret, frame = cap[i].read()
+        cv2.imshow(f'frame{i}', frame)
+        faceCounts[i] = len(face_recognition.face_locations(frame))
+        i += 1
+        print(f'Face count: {faceCounts}')
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+
+for cam in cap:
+    cam.release()
 cv2.destroyAllWindows()
