c164c00651a7ad38d9dca9bb687f8b655d7e374c
guttman <37619448+guttman10@users.noreply.github.com>
2020-02-20 20:57:25 +0200

    Finished busy status for alpha

--- Camera Handler.py
+++ Camera Handler.py
@@ -1,25 +1,66 @@
 import cv2
 import face_recognition
+
 from MotionDetector import GetMotion
 from ImageComapre import compare
-import numpy as np
-#pip install opencv-python
+import threading
+import time
+# pip install opencv-python
 
-cap = cv2.VideoCapture(0)
-_, mainFrame = cap.read()
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    ret, frame2 = cap.read()
-    score, count = compare(mainFrame, frame2, 255*20)
-    mov = GetMotion(frame, frame2)
-    # Display the resulting frame
-    cv2.imshow('Main', frame)
-    if score < 0.95:
-        print(score, count, mov)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+def getBusyStatus(diff, maxCount, count):
+    val = 0
+    if maxCount != 0:
+        val = (1 - diff) * (count/maxCount)
+    print(val)
+    return val
+
+
+def Start(dataToSet):
+    cameraPort = 0
+    cap = cv2.VideoCapture(cameraPort)
+    _, mainFrame = cap.read()
+    totalCounts = 0
+    countInstanced = 0
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret:
+            ret2, frame2 = cap.read()
+        # if everything was captured correctly continue
+        if ret2:
+            score, count = compare(mainFrame, frame2)
+            mov = GetMotion(frame, frame2)
+            if score < 0.95 and mov:
+                totalCounts += count
+                countInstanced += 1
+                # test and set Max count
+                if count > dataToSet["maxCount"]:
+                    dataToSet["maxCount"] = count
+                # set the mean
+                dataToSet["meanCount"] = totalCounts / countInstanced
+                # set the current count
+                dataToSet["currCount"] = count
+            busyTemp = getBusyStatus(score, dataToSet["maxCount"], dataToSet["currCount"])
+            if busyTemp < 1/6:
+                dataToSet["busy"] = "Almost empty"
+            elif busyTemp < 3/6:
+                dataToSet["busy"] = "Somewhat vacant"
+            else:
+                dataToSet["busy"] = "Almost Full"
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+def send_data(name, sendData):
+    while True:
+        print("the data that was suppose to be sent is", sendData)
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    data = {"maxCount": 0, "currCount": 0, "meanCount": 0}
+    tServer = threading.Thread(target=send_data, args=("Server Thread", data))
+    tServer.daemon = True
+    tServer.start()
+    Start(data)
--- ImageComapre.py
+++ ImageComapre.py
@@ -1,33 +1,33 @@
 from skimage.metrics import structural_similarity
 import cv2
 import numpy as np
+from MotionDetector import GetMotion
 
 
-def compare(a, b ,x):
+
+
+def compare(a, b):
+    # a frame 1
+    # b frame 2
     before = a
     after = b
     before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
     after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
     count = 0
     (score, diff) = structural_similarity(before_gray, after_gray, full=True)
-    #show the image with several difference measurements
+    # show the image with several difference measurements
     diff = (diff * 255).astype("uint8")
     thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
     mask = np.zeros(before.shape, dtype='uint8')
-    filled_after = after.copy()
+    contoursimage = after.copy()
     for c in contours:
         area = cv2.contourArea(c)
-        if area > x:
+        if area > 2550:
             count += 1
-            x,y,w,h = cv2.boundingRect(c)
-            cv2.rectangle(filled_after, (x, y), (x + w, y + h), (36,255,12), 2)
-            cv2.drawContours(mask, [c], 0, (0,255,0), -1)
-
-    cv2.imshow('after', filled_after)
-    cv2.imshow('mask',mask)
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(contoursimage, (x, y), (x + w, y + h), (36, 255, 12), 2)
+    cv2.imshow("frame", contoursimage)
     cv2.waitKey(1)
     return score, count
-
-
